91eea6bf81ad818a74745b400caf8b31b54072bf
Monir Hossain <monirhossainpro1@gmail.com>
2021-04-14 15:13:39 +0600

    Add nodemon and ES Module

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const products = require('./productData/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './productData/products.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -14,4 +17,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server is running on 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
--- readme.md
+++ readme.md
@@ -3,6 +3,15 @@
 ```
 npm install react-bootstrap bootstrap
 npm i react-router-dom react-router-bootstrap
+npm i -D nodemon concurrently
 
 
 ```
+
+### Install axios in frontend
+
+````
+npm i axios
+
+```
+````
